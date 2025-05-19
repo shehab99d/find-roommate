62caf5adaefe81762b8483cdc12dd5561ba3acca
shehab99b <shehabulislam429@gmail.com>
2025-05-20 01:25:11 +0600

    my first commit added the tailwind daisy and react router

---
+++ src/Components/Router/Router.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <p>hello world</p>
+    }
+]);
+export default router
